a2560e844c58b7fa1fc21301b5a6ef5b2e229494
alan <acharbonneau@gmail.com>
2017-08-20 18:56:46 +0200

    update tester

--- tester.js
+++ tester.js
@@ -1,7 +1,7 @@
  var cmd=require('node-cmd');
  
     cmd.get(
-        'echo "abc" | python3 punctuator.py Model_Demo-Europarl-EN.pcl > /dev/stdout',
+        'echo "abc" | python3 punctuator.py Model_Demo-Europarl-EN.pcl -',
         function(err, data, stderr){
             console.log('output : ',data)
         }
